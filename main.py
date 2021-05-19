867519196273ec49a0e5dedc99fb6c168357fb7a
VECHAPAVANKUMAR <vechapavan1997@msitprogram.net>
2021-05-19 19:18:56 +0530

    Implemented Tan Operation

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import math
+
+def tan_val(num) :
+    if str(num).isdidgit :
+        return math.tan(num)
+    else :
+        return 'Arguments should be number type'
 def __init__():
-    pass
+    tan_value = tan_val(10)
+    print('Tan value of number 10 is : ' + str(tan_val))
+    
