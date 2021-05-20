253db35b4ee1cd02b34f29db2f229c902bb6fb05
prem2597 <prembammidi25@msitprogram.net>
2021-05-20 19:16:21 +0530

    test function for subtraction

---
+++ test_main.py
@@ -0,0 +1,11 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+    def test_subtraction(self):
+        self.assertEqual(calc.subtraction(5, 5), 0)
+        self.assertEqual(calc.subtraction(-3, 3), -6)
+        self.assertEqual(calc.subtraction(-5, -5), 0)
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
