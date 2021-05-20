75ab4a53ccec6e5ed3e0ca78843e6fcd4328418c
LaxmiNarayanaMurthyVemuri <murthyvemuri@msitprogram.net>
2021-05-20 19:18:13 +0530

    added new testcases

--- test_main.py
+++ test_main.py
@@ -2,6 +2,7 @@ from main import isArmstrong
 
 def test_armstrong():
     assert isArmstrong(153) == True
-    assert isArmstrong(153) == False
+    assert isArmstrong(154) == False
+    assert isArmstrong(22) == False
 
 test_armstrong()
